e10d08411442fdb529fd42c695a455da7caceb9a
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-05-12 16:53:50 +0200

    Fix test

--- tests/test_uniprot.py
+++ tests/test_uniprot.py
@@ -1,9 +1,9 @@
 from textwrap import dedent
 
-from protein_detective.uniprot import Query, build_sparql_query
+from protein_detective.uniprot import Query, _build_sparql_query_pdb
 
 
-def test_build_sparql_query():
+def test__build_sparql_query_pdb():
     # Test with a simple query
     query = Query(
         taxon_id="9606",
@@ -12,9 +12,9 @@ def test_build_sparql_query():
         subcellular_location_go="GO:0005634",  # Cellular component - Nucleus
         molecular_function_go="GO:0003677",  # Molecular function - DNA binding
     )
-    result = build_sparql_query(query, limit=10)
+    result = _build_sparql_query_pdb(query, limit=10)
 
-    expected = dedent("""\
+    expected = dedent("""
         PREFIX up: <http://purl.uniprot.org/core/>
         PREFIX taxon: <http://purl.uniprot.org/taxonomy/>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -22,11 +22,11 @@ def test_build_sparql_query():
         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
         PREFIX GO:<http://purl.obolibrary.org/obo/GO_>
 
-        SELECT DISTINCT ?protein ?pdb_db ?pdb_method ?pdb_resolution ?pdb_chain ?af_db
+        SELECT DISTINCT ?protein ?pdb_db ?pdb_method ?pdb_resolution ?pdb_chain
         WHERE {
+
             # --- Protein Selection ---
             ?protein a up:Protein .
-
             ?protein up:organism taxon:9606 .
             ?protein up:reviewed true .
 
@@ -46,21 +46,14 @@ def test_build_sparql_query():
             ?protein up:classifiedWith|(up:classifiedWith/rdfs:subClassOf) GO:0003677 .
 
 
-            # --- Optional PDB Info ---
-            OPTIONAL {
-                ?protein rdfs:seeAlso ?pdb_db .
-                ?pdb_db up:database <http://purl.uniprot.org/database/PDB> .
-                ?pdb_db up:method ?pdb_method .
-                ?pdb_db up:chainSequenceMapping ?chainSequenceMapping .
-                ?chainSequenceMapping up:chain ?pdb_chain .
-                OPTIONAL { ?pdb_db up:resolution ?pdb_resolution . }
-            }
+            # --- PDB Info ---
+            ?protein rdfs:seeAlso ?pdb_db .
+            ?pdb_db up:database <http://purl.uniprot.org/database/PDB> .
+            ?pdb_db up:method ?pdb_method .
+            ?pdb_db up:chainSequenceMapping ?chainSequenceMapping .
+            ?chainSequenceMapping up:chain ?pdb_chain .
+            OPTIONAL { ?pdb_db up:resolution ?pdb_resolution . }
 
-            # --- Optional AlphaFoldDB Info ---
-            OPTIONAL {
-                ?protein rdfs:seeAlso ?af_db .
-                ?af_db up:database <http://purl.uniprot.org/database/AlphaFoldDB> .
-            }
         }
         LIMIT 10
     """)
