cac45f22b0955c5ea11680d93089db1b104390e7
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-06-18 15:18:35 +0200

    Allow structure path as str + added streamlit and html renderer

--- src/protein_detective/visualization.py
+++ src/protein_detective/visualization.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 from typing import Literal
 
-from molviewspec import create_builder, molstar_notebook
+from molviewspec import create_builder, molstar_html, molstar_notebook, molstar_streamlit
 
 
 def show_structure_and_density(
-    structure: Path, density: Path, renderer: Literal["html", "notebook", "streamlit"] = "notebook"
+    structure: Path | str, density: Path, renderer: Literal["html", "notebook", "streamlit"] = "notebook"
 ):
+    structure = Path(structure)
     builder = create_builder()
     builder.download(url=structure.name).parse(format="pdb").model_structure().component().representation().color(
         color="blue"
@@ -21,6 +22,10 @@ def show_structure_and_density(
 
     if renderer == "notebook":
         return molstar_notebook(state=state, data=data)
+    if renderer == "html":
+        return molstar_html(state=state, data=data)
+    if renderer == "streamlit":
+        return molstar_streamlit(state=state, data=data)
 
     msg = f"Renderer '{renderer}' is not implemented. Supported: 'notebook'."
     raise NotImplementedError(msg)
