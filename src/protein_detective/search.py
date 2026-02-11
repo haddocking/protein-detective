ad943be0de7dd75f0292127aff5a2c5712a354e6
sverhoeven <s.verhoeven@esciencecenter.nl>
2026-02-11 14:17:37 +0100

    Fix docstring/attribute mismatch

--- src/protein_detective/search.py
+++ src/protein_detective/search.py
@@ -14,7 +14,7 @@ class UniprotQuery(Query):
 
     Parameters:
         interaction_partner_seeds: A set of UniProt accessions to search for interaction partners.
-        interaction_partners_excludes: A set of UniProt accessions to exclude from interaction partner results.
+        interaction_partner_excludes: A set of UniProt accessions to exclude from interaction partner results.
         min_residues: Minimum number of residues required in the chain mapped to the UniProt accession.
         max_residues: Maximum number of residues allowed in chain mapped to the UniProt accession.
     """
