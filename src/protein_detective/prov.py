ba54293016c55236c1ebe995013bf97eb88789a6
sverhoeven <s.verhoeven@esciencecenter.nl>
2026-02-11 15:46:38 +0100

    Replace copy of recorded_argparse with call to original with parser_argument

--- src/protein_detective/prov.py
+++ src/protein_detective/prov.py
@@ -1,10 +1,7 @@
 from argparse import Namespace
 from collections.abc import Callable
-from datetime import UTC, datetime
-from functools import wraps
-from pathlib import Path
 
-from rocrate_action_recorder import IOArgumentNames, record_argparse
+from rocrate_action_recorder import recorded_argparse
 
 
 def prov[T](
@@ -21,42 +18,13 @@ def prov[T](
     This is a copy of [rocrate_action_recorder.recorded_argparse][] with parser embedded into args.
     Made to avoid circular import between cli and powerfit.cli modules.
     """
-
-    def decorator(func: Callable[[Namespace], T]) -> Callable[[Namespace], T]:
-        @wraps(func)
-        def wrapper(args: Namespace) -> T:
-            start_datetime = datetime.now(tz=UTC)
-
-            result = func(args)
-
-            parser = args._parser
-            enabled_argument = "prov"
-            dataset_license = "CC BY 4.0"
-            if enabled_argument is None or getattr(args, enabled_argument, False):
-                my_crate_dir = None
-                if crate_dir_argument:
-                    args_crate_dir = getattr(args, crate_dir_argument, None)
-                    if args_crate_dir is not None:
-                        my_crate_dir = Path(args_crate_dir)
-                end_time = datetime.now(tz=UTC)
-                ios = IOArgumentNames(
-                    input_dirs=input_dirs or [],
-                    output_dirs=output_dirs or [],
-                    input_files=input_files or [],
-                    output_files=output_files or [],
-                )
-                record_argparse(
-                    parser=parser,
-                    ns=args,
-                    ios=ios,
-                    start_time=start_datetime,
-                    end_time=end_time,
-                    crate_dir=my_crate_dir,
-                    dataset_license=dataset_license,
-                )
-
-            return result
-
-        return wrapper
-
-    return decorator
+    return recorded_argparse(
+        input_dirs=input_dirs,
+        output_dirs=output_dirs,
+        input_files=input_files,
+        output_files=output_files,
+        crate_dir_argument=crate_dir_argument,
+        enabled_argument="prov",
+        parser_argument="_parser",
+        dataset_license="CC BY 4.0",
+    )
